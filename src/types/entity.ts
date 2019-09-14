3df79b926a475aa5f2f4bca6eef52163ecdabf01
Michael Peng <a625026338@outlook.com>
2019-09-14 11:46:43 +0800

    set up framework

---
+++ src/types/entity.ts
@@ -0,0 +1,11 @@
+export default class Entity {
+    id: number;
+    attack: number;
+    life: number;
+
+    constructor() {
+        this.id = 0;
+        this.attack = 0;
+        this.life = 0;
+    }
+}
