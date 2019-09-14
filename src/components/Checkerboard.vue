c0162968a04139f18ccfb60fddf73ff4acc4b13d
Michael Peng <a625026338@outlook.com>
2019-09-14 22:13:38 +0800

    Finish header

---
+++ src/components/Checkerboard.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-container>
+    <el-header>
+      <b>Hearthstone Helper</b>
+    </el-header>
+    <el-main>Main</el-main>
+  </el-container>
+</template>
+
+<script>
+  export default {
+    name: "Checkerboard"
+  }
+</script>
+
+<style>
+  .el-header {
+    color: #1fab89;
+    line-height: 60px;
+    font-size: 1.5em;
+    border: 1px solid #eee
+  }
+
+  .el-main {
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+</style>
--- src/types/entity.ts
+++ src/types/entity.ts
@@ -1,11 +1,11 @@
 export default class Entity {
-    id: number;
-    attack: number;
-    life: number;
+  id: number;
+  attack: number;
+  life: number;
 
-    constructor() {
-        this.id = 0;
-        this.attack = 0;
-        this.life = 0;
-    }
+  constructor() {
+    this.id = 0;
+    this.attack = 0;
+    this.life = 0;
+  }
 }
