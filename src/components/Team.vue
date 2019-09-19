67ab3ceae9317bf05a4051b167dfcf5f6089321e
Michael Peng <a625026338@outlook.com>
2019-09-19 12:32:29 +0800

    Add Team

---
+++ src/components/Team.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-row class="row" type="flex" justify="center">
+    11
+  </el-row>
+</template>
+
+<script>
+  export default {
+    name: "Team"
+  }
+</script>
+
+<style>
+  .row {
+    height: 300px;
+    background-color: #1fab89;
+  }
+</style>
